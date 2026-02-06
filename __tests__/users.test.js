069cd565c0e71a9a251b66e400c5e8013cb857bf
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-06 16:25:48 +0000

    add test suite for users route - not passing - no husky

---
+++ __tests__/users.test.js
@@ -0,0 +1,78 @@
+const app = require('../app')
+const db = require('../db/connection')
+const seed = require('../db/seeds/seed')
+const data = require('../db/data/test-data')
+const request = require("supertest")
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+/// USERS TESTS ///
+describe("/api/users" , () => 
+  {
+    // Should be vailable on  /api/users 
+    test.only('GET: 200:  "/api/users" is available and retruns message', ()=> 
+      {
+        return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body }) => 
+          {
+            console.log(body)
+            expect(body).toHaveProperty('msg');
+            expect(body.msg).toBe('users route working')
+          });
+        });
+        // get all users
+        test('GET: 200: ... should return an object with key of users' , ()=>
+          {
+            return request(app)
+            .get('/api/users')
+            .expect(200)
+            .then(({ body }) => 
+              {
+                expect(body).toHaveProperty('users')
+              });
+          });
+
+        // return an object with key of users
+    test('GET: 200: ... object should have a key of users with an array as the value', ()=>
+      {
+        return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body }) => 
+          {
+            expect(body.users).toBeArray() 
+          }) 
+      });
+
+        // each object to contain username / name / avatar_url
+    test(`GET: 200: ... the array should contain user objects with the following properties and types:
+            username : (string)
+            name : (string) 
+            avatar_url : (string)` , ()=>
+              {
+                return request(app)
+                .expect(200)
+                .then(({body})=>
+                  {
+                    const { users } = body;
+                    users.forEach((user)=>
+                      {
+                        expect(user).toHaveProperty('username')
+                        expect(typeof user.usename).toBe('string')
+                        expect(user).toHaveProperty('name')
+                        expect(typeof user.usename).toBe('string')
+                        expect(user).toHaveProperty('avatar_url')
+                        expect(typeof user.usename).toBe('string')
+                      }) 
+                  })
+              });
+  });
+
