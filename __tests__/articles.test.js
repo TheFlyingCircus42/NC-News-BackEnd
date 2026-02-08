38da007e7530a2d336f9c68fca467a19c542f64e
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-08 11:19:45 +0000

    write test block for fetch article comments by article ID (no husky)

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -3,6 +3,7 @@ const db = require('../db/connection')
 const seed = require('../db/seeds/seed')
 const data = require('../db/data/test-data')
 const request = require("supertest")
+const comments = require('../db/data/test-data/comments')
 
 beforeEach(() => {
   return seed(data);
@@ -117,19 +118,20 @@ describe("GET: /api/articles" , () =>
 
     });
 
+/// TEST FOR 004 GET ARTICLE BY ID
 describe("GET: /api/articles/:article_id", ()=> 
     {
         test("GET: 200: - route is availabel" , () => 
             {
                 return request(app)
-                .get('/api/:article_id')
+                .get('/api/articles/1')
                 .expect(200)
             })
 
         test("GET: - should return an object with a key of article" , () => 
             {
                 return request(app)
-                .get('/api/:article_id')
+                .get('/api/articles/2')
                 .expect(200)
                 .then(({ body }) => 
                     {
@@ -140,15 +142,29 @@ describe("GET: /api/articles/:article_id", ()=>
         test("GET: - the article key should conatin a single article object", ()=> 
             {
                 return request(app)
-                .get('/api/:article_id')
+                .get('/api/articles/3')
                 .expect(200)
                 .then(({ body }) => 
                     {
                         const { article } = body
                         expect(article).toBeArray()
                         expect(typeof article[0]).toBe('object')//<<<--?
+                        expect(article.length).toBe(1)
                     })
 
+            })
+
+        test("GET: - the article_id should match the requested ID no.", ()=> 
+            {
+                return request(app)
+                .get('/api/articles/5')
+                .expect(200)
+                .then(({ body })=> 
+                    {
+                        const { article } = body
+                        console.log(">>>" , article , '<<<')
+                        expect(article[0].article_id).toBe(5)
+                    })
             })
 
 
@@ -163,20 +179,123 @@ describe("GET: /api/articles/:article_id", ()=>
             article_img_url` , () => 
                 {
                 return request(app)
-                .get('/api/:article_id')
+                .get('/api/articles/4')
                 .expect(200)
                 .then(({ body }) => 
                     {
                         const { article } = body
-                        expect(typeof article.author).toBe('string')
-                        expect(typeof article.title).toBe('string')
-                        expect(typeof article.article_id).toBe('number')
-                        expect(typeof article.body).toBe('string')
-                        expect(typeof article.topic).toBe('string')
-                        expect(typeof article.created_at).toBe('string')
-                        expect(typeof article.votes).toBe('number')
-                        expect(typeof article.article_img_url).toBe('string')
+                        expect(typeof article[0].author).toBe('string')
+                        expect(typeof article[0].title).toBe('string')
+                        expect(typeof article[0].article_id).toBe('number')
+                        expect(typeof article[0].body).toBe('string')
+                        expect(typeof article[0].topic).toBe('string')
+                        expect(typeof article[0].created_at).toBe('string')
+                        expect(typeof article[0].votes).toBe('number')
+                        expect(typeof article[0].article_img_url).toBe('string')
 
                     })
                 })
     })
+
+/// TASK 05 - Get all comments by article ID
+describe("GET: /api/articles/:article_id/comments" , ()=> 
+    {
+        test('GET: 200: - /api/1/comments - route is availabe',()=>
+            {
+                
+                return request(app)
+                .get('/api/articles/1/comments')
+                .expect(200)
+                
+            });
+
+        test('GET: 200: - responds with an object, with a key of "comments" - which has the value of an array' , ()=>
+            {
+                return request(app)
+                .get('/api/articles/3/comments')
+                .expect(200)
+                .then(({ body }) => 
+                    {
+                        expect(body).toHaveProperty("comments")
+                    });
+            });
+
+        test('GET: 200: - the array on the comments key contains comment objects (if there any matches)' , ()=>
+            {
+               return request(app)
+                .get('/api/articles/5/comments')
+                .expect(200)
+                .then(({ body }) => 
+                    {
+                        const { comments } = body
+                        expect(comments).toBeArray()
+                        if (comments.lenght > 0)
+                            {
+                                expect(typeof comments[0]).toBe('object')
+                                expect(comments.length).toBe(1)
+                            }
+                    }); 
+            });
+
+        test('GET: 200: - if there are no comment matches then should return an empty array' , ()=>
+            {
+                return request(app)
+                .get('/api/articles/2/comments')
+                .expect(200)
+                .then(({ body }) => 
+                    {
+                        if( comments.length > 0)
+                            {
+                                const { comments } = body;
+                                expect(comments.length).toBe(0)
+                            }
+                    });
+            });
+
+        test(`GET: 200: - comment objects should have the following properties and values;
+                    comment_id - number
+                    votes - number
+                    created_at - string
+                    author - string
+                    body - string
+                    article_id - number` , () => 
+                   {
+                        return request(app)
+                        .get('/api/articles/1/comments')
+                        .expect(200)
+                        .then(({ body }) => 
+                        {
+                            const { comments } = body;
+                            comments.forEach(comment => 
+                                {
+                                    expect(typeof comment.comment_id).toBe('number')
+                                    expect(typeof comment.votes).toBe('number')
+                                    expect(typeof comment.created_at).toBe('string')
+                                    expect(typeof comment.author).toBe('string')
+                                    expect(typeof comment.body).toBe('string')
+                                    expect(typeof comment.article_id).toBe('number')
+                                });
+
+                        });
+                    });
+
+        test('GET: 200: - the article ID on each comment object should match the requested article ID (if there are any matches)' , () => 
+            {
+                return request(app)
+                .get('/api/articles/1/comments')
+                .expect(200)
+                .then(({ body }) => 
+                    {
+                        const { comments } = body
+                        comments.forEach(comment => 
+                            {
+                                  expect(comment.article_id).toBe(1)
+                            });
+                      
+                    });
+
+            });
+
+        // test('GET: 200: - should return a message if no comments match teh article ID', ()=> {})
+    });
+
